9cba63f4f29589d82d7a39589fac1b4229130664
tengjiang <791096114@qq.com>
2018-05-21 17:56:44 +0800

    创建页面头部

--- src/main/java/com/teng/controller/V2Controller.java
+++ src/main/java/com/teng/controller/V2Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class V2Controller {
 
-    @RequestMapping(value = "index")
+    @RequestMapping(value="index")
     public String index(){
         return "index";
     }
