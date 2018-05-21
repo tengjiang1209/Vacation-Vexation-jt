d6d8bc9da72f103c52412815ef25dc269190cd64
tengjiang <791096114@qq.com>
2018-05-21 15:10:42 +0800

    引入layui

--- src/main/java/com/teng/controller/V2Controller.java
+++ src/main/java/com/teng/controller/V2Controller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BkController {
+public class V2Controller {
 
     @RequestMapping(value = "index")
     public String index(){
